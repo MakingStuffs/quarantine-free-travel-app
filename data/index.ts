02e75f80df20cd1b5b20d76cc8422f94571f5e1f
Paul Singh <33229318+MakingStuffs@users.noreply.github.com>
2022-01-28 19:16:53 +0000

    Update

--- data/index.ts
+++ data/index.ts
@@ -1,10 +1,10 @@
-import { CombinedPageData } from "types";
+import { CombinedPageData, PossibleCountriesData } from "types";
 import _possibleCountries from "./possibleCountries.json";
 import _covidPageUrls from "./covidPageUrls.json";
 import _countryPageUrls from "./countryPageUrls.json";
 import _combinedPages from "./combinedPages.json";
 
-export const possibleCountries: CombinedPageData[] = _possibleCountries;
+export const possibleCountries: PossibleCountriesData[] = _possibleCountries;
 export const combinedPages: CombinedPageData[] = _combinedPages;
 export const countryPageUrls: string[] = _countryPageUrls;
 export const covidPageUrls: string[] = _covidPageUrls;
