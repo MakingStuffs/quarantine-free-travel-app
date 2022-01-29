0155ab02e3c23317e75eba16db09530fe8557215
Paul Singh <33229318+MakingStuffs@users.noreply.github.com>
2022-01-29 10:25:33 +0000

    Implement the global colours component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
-import { GlobalStyles } from "styles";
+import { GlobalStyles, GlobalColors } from "styles";
 import type { AppProps } from "next/app";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <GlobalStyles />
+      <GlobalColors />
       <Component {...pageProps} />
     </>
   );
