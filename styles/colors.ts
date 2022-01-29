037bba55485f9765f67609e64d5e05e7d5259bdf
Paul Singh <33229318+MakingStuffs@users.noreply.github.com>
2022-01-29 10:27:59 +0000

    Add global style variables

---
+++ styles/colors.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalColors = createGlobalStyle`
+    :root {
+        --primary: #289DE7;
+        --primary-dark: #1479B8;
+        --accent-1: #83C1DE;
+        --accent-2: #5F74B0;
+        --accent-3: #1F2F57;
+        --light: #F9F6F6;
+        --dark: #041825;
+        --grey: #1F1F1F;
+    }
+`;
