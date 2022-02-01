ac11b87485824602fa3bb99f99d41211ee6c2646
Paul Singh <33229318+MakingStuffs@users.noreply.github.com>
2022-02-01 08:01:59 +0000

    Test env - 5

--- services/db/getAllRecords.service.ts
+++ services/db/getAllRecords.service.ts
@@ -2,9 +2,13 @@ import { connectToMongo } from "lib";
 
 const getAllRecords = async () => {
   try {
+    console.log("pre");
     const { db } = await connectToMongo();
+    console.log("post");
     const collection = db.collection(process.env.MONGO_COLLECTION as string);
+    console.log("collection");
     const records = await collection.find().toArray();
+    console.log("records");
     return records;
   } catch (e) {
     console.warn(e);
