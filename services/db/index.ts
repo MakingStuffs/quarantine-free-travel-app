ba87679f1d85696fb82d37c3358e7573038b0db8
Paul Singh <33229318+MakingStuffs@users.noreply.github.com>
2022-02-02 08:00:56 +0000

    Add updateAllRecords

--- services/db/index.ts
+++ services/db/index.ts
@@ -3,3 +3,4 @@ export * from "./getAllCountryPageRecords.service";
 export * from "./getAllCovidPageRecords.service";
 export * from "./getAllRecords.service";
 export * from "./updateCountryRecord.service";
+export * from "./updateAllRecords.service";
