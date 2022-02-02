fb5d028ec1d75904c6b966f630a71cb4a763a7ad
Paul Singh <33229318+MakingStuffs@users.noreply.github.com>
2022-02-02 07:57:33 +0000

    Remove unused import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
-import { CountryInterface, PossibleCountriesData } from "types";
+import { CountryInterface } from "types";
 import styled from "styled-components";
 import { useEffect, useState } from "react";
 const ItemMeta: React.FC<{ data: CountryInterface }> = ({ data }) => {
