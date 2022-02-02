54617dc2666b05d6cf16e10dbc0c08cccda4d121
Paul Singh <33229318+MakingStuffs@users.noreply.github.com>
2022-02-02 07:59:17 +0000

    Add type defs to response and add response to error handler [2] -- import type [18 - 22] -- define typed response [27 - 32] -- return error response

--- pages/api/covid-pages.ts
+++ pages/api/covid-pages.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { CountryInterface } from "types";
+import { CountryInterface, SimpleApiResponse } from "types";
 import Cors from "cors";
 import { runMiddleware } from "utils";
 import { getAllCovidPageRecords } from "services";
@@ -15,14 +15,21 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const records: CountryInterface[] =
       (await getAllCovidPageRecords()) as unknown[] as CountryInterface[];
-    // return it
-    res.status(200).json({
+    const response: SimpleApiResponse = {
       data: records,
       message: `${records.length} COVID 19 pages found.`,
       length: records.length,
-    });
+    };
+    // return it
+    res.status(200).json(response);
   } catch (e) {
     console.log(e);
+    const response: SimpleApiResponse = {
+      data: [],
+      message: "There was a problem with our server",
+      length: 0,
+    };
+    res.status(500).json(response);
   }
 };
 
