8fe2a3e3d5627629ae5d1b3b47472e2d34563c8a
Paul Singh <33229318+MakingStuffs@users.noreply.github.com>
2022-02-08 08:26:18 +0000

    Add test environment with textencoder and decoder defined

--- jest.config.ts
+++ jest.config.ts
@@ -22,7 +22,7 @@ const config = {
     "^@/components/(.*)$": "<rootDir>/components/$1",
   },
   testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
-  testEnvironment: "jsdom",
+  testEnvironment: "<rootDir>/jest-env.ts",
   transform: {
     /* Use babel-jest to transpile tests with the next/babel preset
       https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object */
