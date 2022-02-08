75fa9ff557e95bc6d82879d1c1f2d6f9bb519c3a
Paul Singh <33229318+MakingStuffs@users.noreply.github.com>
2022-02-08 08:27:18 +0000

    Update constants

--- config/constants.ts
+++ config/constants.ts
@@ -1,8 +1,12 @@
-const { MONGO_URI, MONGO_DB } = process.env;
+const { MONGO_URI, MONGO_DB, BASE_URL, MONGO_COLLECTION, API_SECRET } =
+  process.env;
 
 const constants = {
   MONGO_DB,
   MONGO_URI,
+  BASE_URL,
+  MONGO_COLLECTION,
+  API_SECRET,
 };
 
 export { constants };
