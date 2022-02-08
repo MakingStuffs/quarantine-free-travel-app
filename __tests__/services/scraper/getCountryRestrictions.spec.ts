cb21ed54ec975555ef9329e7503068d0b52ac69c
Paul Singh <33229318+MakingStuffs@users.noreply.github.com>
2022-02-08 19:39:51 +0000

    Add unit test to ensure all object properties are defined

--- __tests__/services/scraper/getCountryRestrictions.spec.ts
+++ __tests__/services/scraper/getCountryRestrictions.spec.ts
@@ -15,4 +15,19 @@ describe("getCountryRestrictions", () => {
     const output = await getCountryRestrictions(FAKE_COUNTRY_ARRAY);
     expect(output).toHaveLength(FAKE_COUNTRY_ARRAY.length);
   });
+
+  it("each item has all the keys needed for the country interface", async () => {
+    const mockFetch = fetch as jest.Mock;
+    mockFetch.mockReturnValue({ text: () => FAKE_COVID_PAGE_DOM });
+    const output = await getCountryRestrictions(FAKE_COUNTRY_ARRAY);
+    output.forEach((item) => {
+      expect(item.name).toBeDefined();
+      expect(item.restrictionData).toBeDefined();
+      expect(item.restrictionData?.mentions).toBeDefined();
+      expect(item.urls).toBeDefined();
+      expect(item.urls.countryPage).toBeDefined();
+      expect(item.urls.covidPage).toBeDefined();
+      expect(item.updated).toBeDefined();
+    });
+  });
 });
