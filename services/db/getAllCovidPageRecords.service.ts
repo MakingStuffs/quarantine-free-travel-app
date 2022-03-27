54ab11970b3cd26d4847ae99981e50a6562257a7
Paul Singh <im.makingstuffs@gmail.com>
2022-03-27 14:21:13 +0100

    Define a return type [3] -- import type def [10] -- Define data type for the records value [15] -- type cast the return type of the toArray method exposed by Mongo's find function [19] -- Return an empty array if there is an error as a safety precaution

--- services/db/getAllCovidPageRecords.service.ts
+++ services/db/getAllCovidPageRecords.service.ts
@@ -1,21 +1,22 @@
 import { constants } from "config";
 import { connectToMongo } from "lib";
+import { CountryInterface } from "types";
 
-const getAllCovidPageRecords = async () => {
+const getAllCovidPageRecords = async (): Promise<CountryInterface[]> => {
   try {
     const { db } = await connectToMongo();
     const collection = db.collection(constants.MONGO_COLLECTION as string);
 
-    const records = await collection
+    const records: CountryInterface[] = (await collection
       .find(
         {},
         { projection: { name: 1, "urls.entryRequirementsPage": 1, _id: 0 } }
       )
-      .toArray();
-
+      .toArray()) as CountryInterface[];
     return records;
   } catch (e) {
     console.warn(e);
+    return [];
   }
 };
 
