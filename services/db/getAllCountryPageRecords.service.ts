52e2e9b963b18e0791883e962275879b087cad9a
Paul Singh <im.makingstuffs@gmail.com>
2022-03-27 14:17:43 +0100

    Properly type the return value of the function [3] -- Import CountryInterface type def [5] -- Assign the return value of the function [9 - 11] -- Assign the data type of records and type cast the return value of the toArray method exposed by mongo's find function [15] -- Add a return value in the cath block as a safety net

--- services/db/getAllCountryPageRecords.service.ts
+++ services/db/getAllCountryPageRecords.service.ts
@@ -1,16 +1,18 @@
 import { constants } from "config";
 import { connectToMongo } from "lib";
+import { CountryInterface } from "types";
 
-const getAllCountryPageRecords = async () => {
+const getAllCountryPageRecords = async (): Promise<CountryInterface[]> => {
   try {
     const { db } = await connectToMongo();
     const collection = db.collection(constants.MONGO_COLLECTION as string);
-    const records = await collection
+    const records: CountryInterface[] = (await collection
       .find({}, { projection: { name: 1, "urls.countryPage": 1, _id: 0 } })
-      .toArray();
+      .toArray()) as CountryInterface[];
     return records;
   } catch (e) {
     console.warn(e);
+    return [];
   }
 };
 
