470b3f8702220cf5e409f039a181ce7be8c8d02e
Paul Singh <im.makingstuffs@gmail.com>
2022-03-28 16:24:00 +0100

    Properly type the return of the function [9] -- Set the return type to be a collection of CountryInterface

--- services/db/getAllRecords.service.ts
+++ services/db/getAllRecords.service.ts
@@ -1,14 +1,19 @@
 import { constants } from "config";
 import { connectToMongo } from "lib";
+import { Collection } from "mongodb";
+import { CountryInterface } from "types";
 
-const getAllRecords = async () => {
+const getAllRecords = async (): Promise<CountryInterface[]> => {
   try {
     const { db } = await connectToMongo();
-    const collection = db.collection(constants.MONGO_COLLECTION as string);
+    const collection: Collection<CountryInterface> = db.collection(
+      constants.MONGO_COLLECTION as string
+    );
     const records = await collection.find().toArray();
     return records;
   } catch (e) {
     console.warn(e);
+    return [];
   }
 };
 
