e747025cecda611f4c54dcbb98a8174f898ca04a
Paul Singh <im.makingstuffs@gmail.com>
2022-03-28 16:58:35 +0100

    Properly type the function and change to use findOneAndUpdate [8] -- Add a return type [12] -- Type the return value of our collection [16] -- Type the return value of findOneAndUpdate [21 - 22] -- Add name and urls [27] -- Pass option to mongo so that it returns the altered doc or upserts a new one [29] -- type the return [32] -- return null if there is an issue

--- services/db/updateCountryRecord.service.ts
+++ services/db/updateCountryRecord.service.ts
@@ -1,33 +1,35 @@
 import { constants } from "config";
 import { connectToMongo } from "lib";
-import { Collection } from "mongodb";
+import { Collection, ModifyResult } from "mongodb";
 import { CountryInterface } from "types";
 
 const updateCountryRecord = async (
   country: CountryInterface
-): Promise<CountryInterface> => {
-  const { db } = await connectToMongo();
-  // Get collection
-  const collection: Collection = db.collection(
-    constants.MONGO_COLLECTION as string
-  );
-  // update record and get new document
-  const record = await collection.findOne({ name: country.name });
-
-  if (record) {
-    const updatedRecord = await collection.findOneAndUpdate(
-      { name: country.name },
-      {
-        $set: {
-          restrictionData: country.restrictionData,
-          updated: country.updated,
-        },
-      }
+): Promise<CountryInterface | null> => {
+  try {
+    const { db } = await connectToMongo();
+    // Get collection
+    const collection: Collection<CountryInterface> = db.collection(
+      constants.MONGO_COLLECTION as string
     );
-    return updatedRecord as unknown as CountryInterface;
-  } else {
-    const newRecord = await collection.insertOne(country);
-    return newRecord as unknown as CountryInterface;
+    // update record and get new document
+    const record: ModifyResult<CountryInterface> =
+      await collection.findOneAndUpdate(
+        { name: country.name },
+        {
+          $set: {
+            name: country.name,
+            urls: country.urls,
+            restrictionData: country.restrictionData,
+            updated: country.updated,
+          },
+        },
+        { returnDocument: "after", upsert: true }
+      );
+    return record as unknown as CountryInterface;
+  } catch (e) {
+    console.warn(e);
+    return null;
   }
 };
 
