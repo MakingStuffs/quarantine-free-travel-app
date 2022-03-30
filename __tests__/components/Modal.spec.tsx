ca32cd72c8abf34da6847f9ea5d44aaa57a506d7
Paul Singh <im.makingstuffs@gmail.com>
2022-03-30 16:48:18 +0100

    Add more unit tests to focus on the basic functionality of the button and its aria-roles

--- __tests__/components/Modal.spec.tsx
+++ __tests__/components/Modal.spec.tsx
@@ -5,13 +5,102 @@ const MODAL_ID = "test-modal";
 const MODAL_BUTTON_ID = "modal-trigger";
 
 describe("Modal", () => {
-  it("will render a modal and a button", () => {
-    const { baseElement } = render(
-      <Modal data-testid={MODAL_ID} openButtonId={MODAL_BUTTON_ID}></Modal>
+  it("will render a modal and a button and a modal", () => {
+    render(
+      <Modal
+        data-testid={MODAL_ID}
+        buttonAttributes={{
+          ["data-testid" as keyof HTMLButtonElement]: MODAL_BUTTON_ID,
+        }}
+      ></Modal>
     );
     expect(screen.getByTestId(MODAL_ID)).toBeInTheDocument();
-    expect(
-      baseElement.querySelector(`#${MODAL_BUTTON_ID}`)
-    ).toBeInTheDocument();
+    expect(screen.getByTestId(MODAL_BUTTON_ID)).toBeInTheDocument();
+  });
+
+  it("will label the modal as aria-hidden by default", () => {
+    render(
+      <Modal
+        data-testid={MODAL_ID}
+        buttonAttributes={{
+          ["data-testid" as keyof HTMLButtonElement]: MODAL_BUTTON_ID,
+        }}
+      ></Modal>
+    );
+    const modal = screen.getByTestId(MODAL_ID);
+    expect(modal).toHaveAttribute("aria-hidden");
+    expect(modal.getAttribute("aria-hidden")).toBe("true");
+  });
+
+  it("will not label the button as aria-disabled by default", () => {
+    render(
+      <Modal
+        data-testid={MODAL_ID}
+        buttonAttributes={{
+          ["data-testid" as keyof HTMLButtonElement]: MODAL_BUTTON_ID,
+        }}
+      ></Modal>
+    );
+    const button = screen.getByTestId(MODAL_BUTTON_ID);
+    expect(button).toHaveAttribute("aria-disabled");
+    expect(button.getAttribute("aria-disabled")).toBe("false");
+  });
+
+  it("will change the modal's aria-hidden value when the button is clicked", () => {
+    render(
+      <Modal
+        data-testid={MODAL_ID}
+        buttonAttributes={{
+          ["data-testid" as keyof HTMLButtonElement]: MODAL_BUTTON_ID,
+        }}
+      ></Modal>
+    );
+    const modal = screen.getByTestId(MODAL_ID);
+    const button = screen.getByTestId(MODAL_BUTTON_ID);
+    expect(modal).toHaveAttribute("aria-hidden");
+    expect(modal.getAttribute("aria-hidden")).toBe("true");
+    button.click();
+    expect(modal.getAttribute("aria-hidden")).toBe("false");
+  });
+
+  it("will disable the button when the modal is not aria-hidden", () => {
+    render(
+      <Modal
+        data-testid={MODAL_ID}
+        buttonAttributes={{
+          ["data-testid" as keyof HTMLButtonElement]: MODAL_BUTTON_ID,
+        }}
+      ></Modal>
+    );
+    const modal = screen.getByTestId(MODAL_ID);
+    const button = screen.getByTestId(MODAL_BUTTON_ID);
+
+    expect(modal.getAttribute("aria-hidden")).toBe("true");
+    expect(button.getAttribute("aria-disabled")).toBe("false");
+    button.click();
+    expect(modal.getAttribute("aria-hidden")).toBe("false");
+    expect(button.getAttribute("aria-disabled")).toBe("true");
+  });
+
+  it("will not toggle the modal if the button is aria-disabled", () => {
+    render(
+      <Modal
+        data-testid={MODAL_ID}
+        buttonAttributes={{
+          ["data-testid" as keyof HTMLButtonElement]: MODAL_BUTTON_ID,
+        }}
+      ></Modal>
+    );
+    const modal = screen.getByTestId(MODAL_ID);
+    const button = screen.getByTestId(MODAL_BUTTON_ID);
+
+    expect(modal.getAttribute("aria-hidden")).toBe("true");
+    expect(button.getAttribute("aria-disabled")).toBe("false");
+    button.click();
+    expect(modal.getAttribute("aria-hidden")).toBe("false");
+    expect(button.getAttribute("aria-disabled")).toBe("true");
+    button.click();
+    expect(modal.getAttribute("aria-hidden")).toBe("false");
+    expect(button.getAttribute("aria-disabled")).toBe("true");
   });
 });
